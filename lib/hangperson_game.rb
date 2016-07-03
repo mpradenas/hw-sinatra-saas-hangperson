daa14cb02f569d57b68e35032e95db8179585c73
mpradenas <aquilexgato@hotmail.com>
2016-07-03 16:50:16 +0000

    my homerwork

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,15 @@
 class HangpersonGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses, :check_win_or_lose
   
-  def initialize(word)
+  def initialize(word, guesses='', wrong_guesses='')
     @word = word
+    @guesses = guesses
+    @wrong_guesses = wrong_guesses
+    @word_with_guesses = @word.gsub(/\w/,'-')
+    @check_win_or_lose = :play
+    @wrong_attempts=0
+    
   end
 
   def self.get_random_word
@@ -17,6 +17,40 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+  
+  def guess(character)
+    if(character=='' || character=~/\W/ || character==nil)
+      
+      raise(ArgumentError) 
+       
+    end
+    check_regex = /#{character}/i
+    
+    if(@guesses !~ check_regex && @wrong_guesses !~ check_regex)
+    
+      if(@word =~ check_regex)
+        @guesses +=  character
+        
+        @word_with_guesses = @word.gsub(/[^#{@guesses}]/i,'-')
+        if @word_with_guesses==@word
+           
+           @check_win_or_lose = :win
+           
+        end 
+      else
+        @wrong_guesses +=  character
+        @wrong_attempts +=1
+        if @wrong_attempts==7
+           
+           @check_win_or_lose = :lose
+        
+        end
+      end
+    else
+      return false
+    end
+    
   end
 
 end
